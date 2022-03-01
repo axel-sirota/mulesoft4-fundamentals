93311a91bd6b6244d175d71b88e354442ca432ed
Axel Sirota <axel.sirota@gmail.com>
2022-03-01 15:13:57 -0300

    finish

---
+++ setup/insert_flights_db.py
@@ -0,0 +1,12 @@
+import pandas as pd
+from sqlalchemy import create_engine
+from config import host, port, database, user, password
+
+conn_str = f"postgresql://{user}:{password}@{host}/{database}"
+engine = create_engine(conn_str)
+with open("flights.csv", 'r') as file:
+    flights = pd.read_csv(file)
+aa_flights = flights[flights.AIRLINE == "AA"]
+dl_flights = flights[flights.AIRLINE == "DL"]
+aa_flights.to_sql('american_flights', con=engine, index=True, index_label='id', if_exists='replace')
+dl_flights.to_sql('delta_flights', con=engine, index=True, index_label='id', if_exists='replace')
