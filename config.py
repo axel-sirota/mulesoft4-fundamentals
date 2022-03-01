4374ebe47f967432e31369fed7143a3984d0047e
Axel Sirota <axel.sirota@gmail.com>
2022-03-01 15:12:57 -0300

    gitignnore

---
+++ config.py
@@ -0,0 +1,7 @@
+host = "ec2-18-215-8-186.compute-1.amazonaws.com"
+port = 5432
+database = "d59ekhl7vgu04b"
+user = "hprojihloozddc"
+password = "a98927921b51f798d3bfeaea6827d4104bf6143440cbfa270e6dd8a1df761c6b"
+URI = "jdbc:postgres://hprojihloozddc:a98927921b51f798d3bfeaea6827d4104bf6143440cbfa270e6dd8a1df761c6b@ec2-18-215-8-186.compute-1.amazonaws.com:5432/d59ekhl7vgu04b"
+className = "org.postgresql.Driver"
